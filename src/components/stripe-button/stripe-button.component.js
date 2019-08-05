6d68e111beb782029f5ff18100fb25b914c2c074
bboti199 <b.boti199@yahoo.com>
2019-08-05 11:37:56 +0300

    add stripe payment

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_w3rl54C2vz3M4gNIOc6Zqgvj000m6ff1JW';
+
+  const onToken = token => {
+    console.log(token);
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
